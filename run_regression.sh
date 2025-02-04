0fab663336ea52d8d572512eff159cdfb6d8242e
Betameche <meven.mognol@gmail.com>
2025-02-04 23:32:07 +0100

    update run_regression.sh to create missing regression tests before compilation

--- run_regression.sh
+++ run_regression.sh
@@ -16,9 +16,19 @@ else
     mkdir regression
 fi
 
-# compile all regression tests in src folder to regression folder
+# create regression tests if they don't exist in src folder
 for file in src/*.cpp; do
-    g++ -o "regression/$(basename "$file" .cpp)" "$file" -O2 -std=c++20
+    if [[ "$file" != *_regress.cpp ]]; then
+        regress_file="src/$(basename "$file" .cpp)_regress.cpp"
+        if [ ! -f "$regress_file" ]; then
+            ./create_regression.sh "$file"
+        fi
+    fi
+done
+
+# compile all regression tests in src folder to regression folder
+for file in src/*_regress.cpp; do
+    g++ -o "regression/$(basename "$file" _regress.cpp)" "$file" -O2 -std=c++20
 done
 
 # run all regression tests
